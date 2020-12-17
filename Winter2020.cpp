1b5d8f348a069e61fc387d4f0ff4928d720c08df
김우찬 <kwc1887@gmail.com>
2020-12-17 23:24:52 +0900

    버튼 및 edit 컨트롤 생성

--- Winter2020.cpp
+++ Winter2020.cpp
@@ -1,18 +1,39 @@
 ﻿#include "pch.h"
 #include "framework.h"
 #include "Winter2020.h"
+#include <stdlib.h>
+#include <windows.h>
 
 #define FILE_MENU_NEW 1
 #define FILE_MENU_OPEN 2
 #define FILE_MENU_EXIT 3
+#define GENERATE_BUTTON 4
 
 HMENU hMenu;
+HWND hName, hAge, hOut;
+
+// 버튼 및 Edit 컨트롤
+void addControls(HWND hWnd)
+{
+	CreateWindowW(L"static", L"Name :", WS_VISIBLE | WS_CHILD, 100, 55, 98, 22, hWnd, NULL, NULL, NULL);
+	hName = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER , 150, 55, 98, 22, hWnd, NULL, NULL, NULL);
+	
+	CreateWindowW(L"static", L"Age :", WS_VISIBLE | WS_CHILD, 100, 95, 98, 22, hWnd, NULL, NULL, NULL);
+	hAge = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER , 150, 95, 98, 22, hWnd, NULL, NULL, NULL);
+	
+	CreateWindowW(L"Button", L"입력 완료", WS_VISIBLE | WS_CHILD | WS_BORDER, 125, 130, 98, 38, hWnd, (HMENU)GENERATE_BUTTON, NULL, NULL);
+	
+	hOut = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER , 100, 200, 300, 200, hWnd, NULL, NULL, NULL);
+
+
+}
 
 // 메뉴
-void addMenus(HWND hWnd)
 {
+	
 	hMenu = CreateMenu(); // 메뉴를 만듦
-	HMENU hFileMenu = CreateMenu();
+	HMENU hFileMenu = CreateMenu
+void addMenus(HWND hWnd)();
 	HMENU hSubMenu = CreateMenu();
 
 	AppendMenu(hSubMenu, MF_STRING, NULL, L"SubMenu Item");
@@ -40,7 +61,7 @@ void addPaint(HWND hWnd)
 	SetTextColor(h_dc, RGB(255, 255, 255)); // 폰트 색
 	SetBkMode(h_dc, TRANSPARENT); // 배경색이랑 폰트 배경색 맞추기
 	HGDIOBJ h_old_font = SelectObject(h_dc, h_font); // 폰트 결정
-	RECT r = { 10, 10, 370, 50 }; // 사각형 위치, 크기
+	RECT r = { 10, 10, 480, 50 }; // 사각형 위치, 크기
 	FillRect(h_dc, &r, (HBRUSH)GetStockObject(DKGRAY_BRUSH)); // 사각형 채우기
 	// 사각형 맞춰서 문자열 쓰기
 	DrawText(h_dc, L"노인복지관 명단", 8, &r, DT_CENTER | DT_SINGLELINE | DT_VCENTER);
@@ -59,7 +80,6 @@ LRESULT CALLBACK WndProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
 {
 	switch (msg)
 	{
-
 	case  WM_PAINT:
 		addPaint(hWnd);
 		break;
@@ -70,12 +90,26 @@ LRESULT CALLBACK WndProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
 			DestroyWindow(hWnd);
 			break;
 		case FILE_MENU_NEW:
-			MessageBeep(MB_ICONINFORMATION);
+			MessageBeep(MB_ICONINFORMATION); // 소리나게
+			break;
+		case GENERATE_BUTTON:
+
+			TCHAR name[30], age[10], out[50];
+			GetWindowText(hName, name, 30);
+			GetWindowText(hAge, age, 10);
+			// 유니코드라서 TCHAR
+			_tcscpy_s(out, 30, name);
+			_tcscat_s(out, L" is ");
+			_tcscat_s(out, age);
+			_tcscat_s(out, L" years old.");
+
+			SetWindowText(hOut, out);
 			break;
 		}
 		break;
 	case WM_CREATE:
 		addMenus(hWnd);
+		addControls(hWnd);
 		break;
 	case WM_DESTROY: // 닫기
 		PostQuitMessage(0);
@@ -109,7 +143,7 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int nCmdSho
 
 	// 윈도우 생성
 	HWND hWnd = CreateWindow(my_class_name, L"ㅋㅋ",
-		WS_OVERLAPPEDWINDOW, 550, 250, 400, 350, NULL, NULL, hInst, NULL);
+		WS_OVERLAPPEDWINDOW, 550, 250, 500, 500, NULL, NULL, hInst, NULL);
 	ShowWindow(hWnd, nCmdShow);
 	UpdateWindow(hWnd);
 
