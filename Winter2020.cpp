51b64a81f8d24d3c725fe7415b74a2df606d6c40
김우찬 <kwc1887@gmail.com>
2020-12-17 21:19:45 +0900

    Revert "Revert "메뉴 바 생성 및 창 만들기 코드 수정""
    This reverts commit 12a948b40a7aee074295642b3032a1295ba3e077.

--- Winter2020.cpp
+++ Winter2020.cpp
@@ -2,55 +2,85 @@
 #include "framework.h"
 #include "Winter2020.h"
 
-// 메시지 처리 함수
-LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+
+HMENU hMenu;
+
+// 메뉴
+void addMenus(HWND hWnd)
 {
-	// 응용프로그램에 VM_QUIT메시지를 전달해서 프로그램 종료
-	if (uMsg == WM_DESTROY) PostQuitMessage(0);
-	else if (uMsg == WM_CLOSE) // x누르면 발생하는 메시지
-	{
-		// 메시지박스 - return은 OK or CANCEL
-		int check = MessageBox(hWnd, L"프로그램을 종료하시겠습니까?"
-			, L"Panorama", MB_ICONQUESTION | MB_OKCANCEL);
-		if (IDCANCEL == check) return 0;
-	}
+	hMenu = CreateMenu(); // 메뉴를 만듦
+	AppendMenu(hMenu, MF_STRING, 1, L"File");
+	AppendMenu(hMenu, MF_STRING, NULL, L"Help");
+	SetMenu(hWnd, hMenu);
+}
 
-	if (uMsg == WM_PAINT)
-	{
-		PAINTSTRUCT ps;
-		HDC h_dc = BeginPaint(hWnd, &ps);
-		//-------------------------------------------
-		
-		HFONT h_font = CreateFont(32, 0, 0, 0, FW_BOLD, FALSE, FALSE, 0, DEFAULT_CHARSET, OUT_DEFAULT_PRECIS,
-			CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH | FF_SWISS, L"굴림체");
-	
-		SetTextColor(h_dc, RGB(255, 255, 255)); // 폰트 색
-		SetBkMode(h_dc, TRANSPARENT); // 배경색이랑 폰트 배경색 맞추기
-		HGDIOBJ h_old_font = SelectObject(h_dc, h_font); // 폰트 결정
-		RECT r = { 10, 10, 370, 50 }; // 사각형 위치, 크기
-		FillRect(h_dc, &r, (HBRUSH)GetStockObject(DKGRAY_BRUSH)); // 사각형 채우기
-		// 사각형 맞춰서 문자열 쓰기
-		DrawText(h_dc, L"노인복지관 명단", 8, &r, DT_CENTER | DT_SINGLELINE | DT_VCENTER );
-		SelectObject(h_dc, h_old_font); // 폰트 설정 끝
-		
-		SetTextColor(h_dc, RGB(0, 0, 0)); // 폰트 컬러
-		TextOut(h_dc, 10, 80, L"안녕안녕", 4);
+// 창 꾸미기
+void addPaint(HWND hWnd)
+{
+	PAINTSTRUCT ps;
+	HDC h_dc = BeginPaint(hWnd, &ps);
 
-		//-------------------------------------------
-	
-		DeleteObject(h_font);
-		EndPaint(hWnd, &ps);
-		return 0;
+	HFONT h_font = CreateFont(32, 0, 0, 0, FW_BOLD, FALSE, FALSE, 0, DEFAULT_CHARSET, OUT_DEFAULT_PRECIS,
+		CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH | FF_SWISS, L"굴림체");
+
+	SetTextColor(h_dc, RGB(255, 255, 255)); // 폰트 색
+	SetBkMode(h_dc, TRANSPARENT); // 배경색이랑 폰트 배경색 맞추기
+	HGDIOBJ h_old_font = SelectObject(h_dc, h_font); // 폰트 결정
+	RECT r = { 10, 10, 370, 50 }; // 사각형 위치, 크기
+	FillRect(h_dc, &r, (HBRUSH)GetStockObject(DKGRAY_BRUSH)); // 사각형 채우기
+	// 사각형 맞춰서 문자열 쓰기
+	DrawText(h_dc, L"노인복지관 명단", 8, &r, DT_CENTER | DT_SINGLELINE | DT_VCENTER);
+	SelectObject(h_dc, h_old_font); // 폰트 설정 끝
+
+	//SetTextColor(h_dc, RGB(0, 0, 0)); // 폰트 컬러
+	//TextOut(h_dc, 10, 80, L"안녕안녕", 4);
+	DeleteObject(h_font);
+	EndPaint(hWnd, &ps);
+	return;
+
+}
+
+// 닫는창 메시지 박스
+int addMessageBox(HWND hWnd) {
+	// 메시지박스 - return은 OK or CANCEL
+	int check = MessageBox(hWnd, L"프로그램을 종료하시겠습니까?"
+		, L"Panorama", MB_ICONQUESTION | MB_OKCANCEL);
+	if (IDCANCEL == check) return 1;
+		
+}
+
+// 메시지 처리 함수
+LRESULT CALLBACK WndProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
+{
+	switch (msg)
+	{
+	case  WM_PAINT:
+		addPaint(hWnd);
+		break;
+	case WM_COMMAND:
+		switch (wp)
+		{
+		case 1:
+			MessageBeep(MB_OK);
+			break;
+		}
+		break;
+	case WM_CREATE:
+		addMenus(hWnd);
+		break;
+	case WM_CLOSE: // 닫는 창
+		if (addMessageBox(hWnd)) PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, msg, wp, lp);
 	}
-	
-	// 내가 처리하지 않은 메시지들의 기본 작업 대신 처리
-	return DefWindowProc(hWnd, uMsg, wParam, lParam);
 
 }
 
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmpLine, int nCmdShow)
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int nCmdShow)
 {
+
 	// 윈도우 클래스 등록
 	WNDCLASS wc;
 
@@ -60,8 +90,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmpLine
 	wc.hbrBackground = (HBRUSH)COLOR_WINDOW; // 배경색
 	wc.hCursor = LoadCursor(NULL, IDC_ARROW); // 마우스 커서
 	wc.hIcon = LoadIcon(NULL, IDI_INFORMATION); // 제목 표시줄 로고
-	wc.hInstance = hInstance; // 첫 번째 매개변수 값 대입
-	wc.lpfnWndProc = WndProc; // 기본 메시지 처리 함수 지정
+	wc.hInstance = hInst; // 첫 번째 매개변수 값 대입
+	wc.lpfnWndProc = WndProcedure; // 기본 메시지 처리 함수 지정
 	wc.lpszClassName = my_class_name; // 'Window class' 이름 지정
 	wc.lpszMenuName = L"NULL"; // 메뉴바 사용x
 	wc.style = CS_HREDRAW | CS_VREDRAW; // 윈도우 그리기 항목 설정
@@ -69,14 +99,14 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmpLine
 	RegisterClass(&wc); // wc 정보 기준으로 'Window class' 등록
 
 	// 윈도우 생성
-	HWND hWnd = CreateWindow(my_class_name, L"Panorama",
-		WS_OVERLAPPEDWINDOW, 550, 250, 400, 350, NULL, NULL, hInstance, NULL);
+	HWND hWnd = CreateWindow(my_class_name, L"ㅋㅋ",
+		WS_OVERLAPPEDWINDOW, 550, 250, 400, 350, NULL, NULL, hInst, NULL);
 	ShowWindow(hWnd, nCmdShow);
 	UpdateWindow(hWnd);
 
 	// 프로그램에 전달된 메시지를 번역/실행
 	MSG msg;
-	while (GetMessage(&msg, NULL, 0, 0))
+	while (GetMessage(&msg, NULL, NULL, NULL))
 	{
 		// 추가 메시지 발생
 		TranslateMessage(&msg);
